82d92403a2b9d65a877fda1ca97c45e274bd2514
tnakaicode <tnakaicode@gmail.com>
2020-08-09 17:20:35 +0900

    reflection and refractive

--- plot_reflect_refract.py
+++ plot_reflect_refract.py
@@ -11,7 +11,7 @@ from optparse import OptionParser
 
 sys.path.append(os.path.join("./"))
 from src.base import plot2d
-from src.index import ref_index
+from src.index import ref_index, ref_ratio
 
 import logging
 logging.getLogger('matplotlib').setLevel(logging.ERROR)
@@ -20,17 +20,35 @@ if __name__ == '__main__':
     argvs = sys.argv
     parser = OptionParser()
     parser.add_option("--dir", dest="dir", default="./")
-    parser.add_option("--pxyz", dest="pxyz",
-                      default=[0.0, 0.0, 0.0], type="float", nargs=3)
+    parser.add_option("--n12", dest="n12",
+                      default=[1.0, 1.5], type="float", nargs=2)
     opt, argc = parser.parse_args(argvs)
     print(opt, argc)
 
     obj = plot2d(aspect="auto")
+    n1, n2 = opt.n12
     pt = np.linspace(0, 1, 100) * 90
-    tp, rp, ts, rs = ref_index(n1=1.0, n2=1.0, t0=pt)
+    [tp, rp, ts, rs], [T_p, R_p, T_s, R_s] = ref_ratio(n1=n1, n2=n2, t0=pt)
+
+    title = "n1={:.2f} -> n2={:.2f}".format(n1, n2)
+    obj.new_2Dfig(aspect="auto")
+    obj.axs.set_title("Reflection and Transmition Index\n" + title)
     obj.axs.plot(pt, tp, label="Transmit P-pol")
     obj.axs.plot(pt, rp, label="Reflect P-pol")
     obj.axs.plot(pt, ts, label="Transmit S-pol")
     obj.axs.plot(pt, rs, label="Reflect S-pol")
     obj.axs.legend()
-    obj.SavePng()
+    obj.axs.set_xlim(0, 90)
+    obj.axs.set_ylim(-1.1, 1.1)
+    obj.SavePng(obj.tempname + "-index.png")
+
+    obj.new_2Dfig(aspect="auto")
+    obj.axs.set_title("Reflection and Transmition Ratio\n" + title)
+    obj.axs.plot(pt, T_p, label="Transmit P-pol")
+    obj.axs.plot(pt, R_p, label="Reflect P-pol")
+    obj.axs.plot(pt, T_s, label="Transmit S-pol")
+    obj.axs.plot(pt, R_s, label="Reflect S-pol")
+    obj.axs.legend()
+    obj.axs.set_xlim(0, 90)
+    obj.axs.set_ylim(-0.1, 1.1)
+    obj.SavePng(obj.tempname + "-ratio.png")
--- src/index.py
+++ src/index.py
@@ -1,13 +1,24 @@
 import numpy as np
 
 
-def ref_index(n1, n2, t0):
+def ref_index(n1=1.0, n2=1.0, t0=0.0):
     t1 = np.deg2rad(t0)
     t2 = np.arcsin((n1 / n2) * np.sin(t1))
-    rs = (n1 * np.cos(t1) - n2 * np.cos(t2)) / \
-        (n1 * np.cos(t1) + n2 * np.cos(t2))
-    ts = 2 * n1 * np.cos(t1) / (n1 * np.cos(t1) + n2 * np.cos(t2))
-    rp = (n2 * np.cos(t1) - n1 * np.cos(t2)) / \
-        (n2 * np.cos(t1) + n1 * np.cos(t2))
-    tp = 2 * n1 * np.cos(t1) / (n2 * np.cos(t1) + n1 * np.cos(t2))
+    cs = n1 * np.cos(t1) + n2 * np.cos(t2)
+    ct = n2 * np.cos(t1) + n1 * np.cos(t2)
+    rs = (n1 * np.cos(t1) - n2 * np.cos(t2)) / cs
+    ts = 2 * n1 * np.cos(t1) / cs
+    rp = (n2 * np.cos(t1) - n1 * np.cos(t2)) / ct
+    tp = 2 * n1 * np.cos(t1) / ct
     return tp, rp, ts, rs
+
+def ref_ratio (n1=1.0, n2=1.0, t0=0.0):
+    t1 = np.deg2rad(t0)
+    t2 = np.arcsin((n1 / n2) * np.sin(t1))
+    tp, rp, ts, rs = ref_index(n1, n2, t0)
+    R_p = rp**2
+    T_p = tp**2*(n2*np.cos(t2)/ (n1*np.cos(t1)))
+    R_s = rs**2
+    T_s = ts**2*(n2*np.cos(t2)/ (n1*np.cos(t1)))
+    return [tp, rp, ts, rs], [T_p, R_p, T_s, R_s]
+
